0bb05d44c7e4f4e7e03d5d69a89ea62dca186013
Antonio <antonioribdev@gmail.com>
2021-11-17 19:49:22 -0300

    wip

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import * as firebase from "firebase-admin";
+import * as firebase from 'firebase-admin';
 
 @Injectable()
 export class FirebaseService {
-    private firebaseApp: firebase.app.App;
+  private firebaseApp: firebase.app.App;
 
-    constructor() {
-        this.firebaseApp = firebase.initializeApp({
-            credential: firebase.credential.cert({
-                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                projectId: process.env.FIREBASE_PROJECT_ID,
-                privateKey: process.env.FIREBASE_PRIVATE_KEY,
-            })
-        });
-    }
+  constructor() {
+    const firebaseCredentials = {
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    };
 
-    getAuth = (): firebase.auth.Auth => {
-        return this.firebaseApp.auth();
-    }
+    this.firebaseApp = firebase.initializeApp({
+      credential: firebase.credential.cert(firebaseCredentials),
+    });
+  }
+
+  getAuth = (): firebase.auth.Auth => {
+    return this.firebaseApp.auth();
+  };
 }
