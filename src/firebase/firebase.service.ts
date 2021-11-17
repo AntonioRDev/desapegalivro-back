aad96d5f8c377fb536a79a3dcece6010aac6d128
Antonio <antonioribdev@gmail.com>
2021-11-17 20:55:25 -0300

    fix: Firebase private keu

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -9,7 +9,7 @@ export class FirebaseService {
     const firebaseCredentials = {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     };
 
     this.firebaseApp = firebase.initializeApp({
