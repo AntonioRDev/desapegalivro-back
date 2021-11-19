b71303f23347c07b56618b1eb2772f7a6bcf6289
Antonio <antonioribdev@gmail.com>
2021-11-19 00:05:31 -0300

    Incrementador de quantidade de propostas recebidas

--- src/application/application.repository.ts
+++ src/application/application.repository.ts
@@ -4,45 +4,58 @@ import { ApplyToReceiveRequest } from './dto/apply-to-receive-request.dto';
 
 @Injectable()
 export class ApplicationRepository {
-    constructor(private readonly prisma: PrismaService) {}
-    
-    async getByUserId(userId: number) {
-        return await this.prisma.application.findMany({
-            where: {
-                userId: userId
-            },
-            include: {
-                book: {
-                    include: {
-                        category: true
-                    }
-                }
-            }
-        })
-    }
+  constructor(private readonly prisma: PrismaService) {}
 
-    async applyToReceive(applyToReceiveDto: ApplyToReceiveRequest) {
-        return await this.prisma.application.create({
-            data: applyToReceiveDto,
-            include: {
-                user: true,
-                book: {
-                    include: {
-                        user: true
-                    }
-                }
-            }
-        })
-    }
+  async getByUserId(userId: number) {
+    return await this.prisma.application.findMany({
+      where: {
+        userId: userId,
+      },
+      include: {
+        book: {
+          include: {
+            category: true,
+          },
+        },
+      },
+    });
+  }
 
-    async setEmailSended(isSended: boolean, applicationId: number) {
-        return await this.prisma.application.update({
-            where: {
-                id: applicationId
-            },
-            data: {
-                isEmailSended: isSended
-            }
-        })
-    }
+  async applyToReceive(applyToReceiveDto: ApplyToReceiveRequest) {
+    const persistedApplication = await this.prisma.application.create({
+      data: applyToReceiveDto,
+      include: {
+        user: true,
+        book: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+
+    await this.prisma.donatedBook.update({
+      where: {
+        id: applyToReceiveDto.bookId,
+      },
+      data: {
+        applicationsQty: {
+          increment: 1,
+        },
+      },
+    });
+
+    return persistedApplication;
+  }
+
+  async setEmailSended(isSended: boolean, applicationId: number) {
+    return await this.prisma.application.update({
+      where: {
+        id: applicationId,
+      },
+      data: {
+        isEmailSended: isSended,
+      },
+    });
+  }
 }
